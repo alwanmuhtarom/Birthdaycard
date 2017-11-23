a0f199d4fb6195acba39598a9667e0103f6d23e1
moehtaroom27 <moehtaroom270895@gmail.com>
2017-11-24 00:13:21 +0700

    0.0.1

---
+++ app/src/main/java/com/example/asus/birthdaycard/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.asus.birthdaycard;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button button = (Button) findViewById(R.id.button1);
+
+        // Show toast message when button is clicked
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getApplicationContext(), "kadonya nyusul ya....", Toast.LENGTH_LONG).show();// Set your own toast  message
+            }
+        });
+    }
+}
